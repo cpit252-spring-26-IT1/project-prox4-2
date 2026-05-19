3fdc6947a4a64558718ce9538158f70e8006638f
Abdulrhman Mari Nasiri <anasiri@stu.kau.edu.sa>
2026-05-19 23:05:02 +0300

    Add new event types to AccessEvent enum #38

--- src/main/java/sa/edu/kau/fcit/cpit252/project/observer/AccessEvent.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/observer/AccessEvent.java
@@ -1,6 +1,26 @@
 package sa.edu.kau.fcit.cpit252.project.observer;
+
 public enum AccessEvent {
+    // File access events
     ACCESS_GRANTED,
     ACCESS_DENIED,
-    LIMIT_REACHED
+    LIMIT_REACHED,
+
+    // Authentication events
+    LOGIN_SUCCESS,
+    LOGIN_FAILED,
+    ACCOUNT_LOCKED,
+    PASSWORD_CHANGED,
+
+    // User management events
+    USER_CREATED,
+    USER_DELETED,
+    USER_PROMOTED,
+    USER_DEMOTED,
+
+    // File management events
+    FILE_REGISTERED,
+    FILE_DELETED,
+    FILE_LOCKED,
+    FILE_UNLOCKED
 }
