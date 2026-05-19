8d2217495033ca35fd9e0ec5ba2c81f33e5ee3af
Nawaf Baryan <Nfbaryan@gmail.com>
2026-05-19 04:55:16 +0300

    Add locked flag to FileResource with getter and setter close #29

--- src/main/java/sa/edu/kau/fcit/cpit252/project/FileResource.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/FileResource.java
@@ -2,16 +2,20 @@ package sa.edu.kau.fcit.cpit252.project;
 
 public class FileResource {
     private final String name;
-    private final String path; 
+    private final String path;
     private final FileType type;
+    private boolean locked;
 
     public FileResource(String name, String path, FileType type) {
         this.name = name;
         this.path = path;
         this.type = type;
+        this.locked = false;
     }
 
     public String getName() { return name; }
     public String getPath() { return path; }
     public FileType getType() { return type; }
+    public boolean isLocked() { return locked; }
+    public void setLocked(boolean locked) { this.locked = locked; }
 }
