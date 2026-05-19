ef938f532b182329f9e02b59a2f1bafe7817bfed
Abdulrhman Mari Nasiri <anasiri@stu.kau.edu.sa>
2026-05-19 23:10:43 +0300

    Update AlertObserver with new event types and Colors close #38

--- src/main/java/sa/edu/kau/fcit/cpit252/project/observer/AlertObserver.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/observer/AlertObserver.java
@@ -3,22 +3,42 @@ package sa.edu.kau.fcit.cpit252.project.observer;
 import sa.edu.kau.fcit.cpit252.project.auth.UserAccount;
 import sa.edu.kau.fcit.cpit252.project.files.FileResource;
 import sa.edu.kau.fcit.cpit252.project.ui.Colors;
+
 public class AlertObserver implements AccessObserver {
 
     @Override
-    public void onAccessEvent(AccessEvent event, User user, FileResource file) {
+    public void onAccessEvent(AccessEvent event, UserAccount user, FileResource file) {
+        String username = user != null ? user.getUsername() : "SYSTEM";
+        String fileName = file != null ? file.getName() : "N/A";
+
         switch (event) {
             case ACCESS_DENIED:
-                System.out.println("!! [SECURITY ALERT] Unauthorized access attempt by '"
-                        + user.getUsername() + "' (Role: " + user.getRole()
-                        + ") on file: " + file.getName());
+                System.out.println(Colors.red("!! [SECURITY ALERT] Unauthorized access attempt by '"
+                        + username + "' on file: " + fileName));
                 break;
             case LIMIT_REACHED:
-                System.out.println("!! [SECURITY ALERT] View limit exceeded by '"
-                        + user.getUsername() + "' for file: " + file.getName()
-                        + ". Further access is blocked.");
+                System.out.println(Colors.red("!! [SECURITY ALERT] View limit exceeded by '"
+                        + username + "' for file: " + fileName));
                 break;
-            case ACCESS_GRANTED:
+            case ACCOUNT_LOCKED:
+                System.out.println(Colors.red("!! [SECURITY ALERT] Account '" + username + "' has been locked."));
+                break;
+            case USER_CREATED:
+                System.out.println(Colors.green(">> [AUDIT] New user created: " + username));
+                break;
+            case USER_PROMOTED:
+                System.out.println(Colors.green(">> [AUDIT] User promoted: " + username));
+                break;
+            case USER_DEMOTED:
+                System.out.println(Colors.yellow(">> [AUDIT] User demoted: " + username));
+                break;
+            case FILE_LOCKED:
+                System.out.println(Colors.yellow(">> [AUDIT] File locked: " + fileName));
+                break;
+            case FILE_UNLOCKED:
+                System.out.println(Colors.green(">> [AUDIT] File unlocked: " + fileName));
+                break;
+            default:
                 break;
         }
     }
