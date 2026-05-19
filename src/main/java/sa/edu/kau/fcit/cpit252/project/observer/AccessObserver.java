b58fc766c97045821adc0b941d9676d0dd85db58
Abdulrhman Mari Nasiri <anasiri@stu.kau.edu.sa>
2026-05-19 23:06:35 +0300

    Update AccessObserver to use UserAccount #38

--- src/main/java/sa/edu/kau/fcit/cpit252/project/observer/AccessObserver.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/observer/AccessObserver.java
@@ -2,6 +2,7 @@ package sa.edu.kau.fcit.cpit252.project.observer;
 
 import sa.edu.kau.fcit.cpit252.project.auth.UserAccount;
 import sa.edu.kau.fcit.cpit252.project.files.FileResource;
+
 public interface AccessObserver {
-    void onAccessEvent(AccessEvent event, User user, FileResource file);
+    void onAccessEvent(AccessEvent event, UserAccount user, FileResource file);
 }
