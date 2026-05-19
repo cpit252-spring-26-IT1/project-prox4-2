cb4a42eab4ad82da54a97a94906f54957d207634
Nawaf Baryan <Nfbaryan@gmail.com>
2026-05-19 06:06:52 +0300

    Create PromotionRequest class with serialization support #43

---
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/PromotionRequest.java
@@ -0,0 +1,38 @@
+package sa.edu.kau.fcit.cpit252.project;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class PromotionRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String requesterUsername;
+    private final Role currentRole;
+    private final Role requestedRole;
+    private final LocalDateTime requestTime;
+    private boolean pending;
+
+    public PromotionRequest(String requesterUsername, Role currentRole) {
+        this.requesterUsername = requesterUsername;
+        this.currentRole = currentRole;
+        this.requestedRole = getNextRole(currentRole);
+        this.requestTime = LocalDateTime.now();
+        this.pending = true;
+    }
+
+    private Role getNextRole(Role role) {
+        switch (role) {
+            case GUEST:   return Role.USER;
+            case USER:    return Role.MANAGER;
+            default:      return role;
+        }
+    }
+
+    public String getRequesterUsername() { return requesterUsername; }
+    public Role getCurrentRole()         { return currentRole; }
+    public Role getRequestedRole()       { return requestedRole; }
+    public LocalDateTime getRequestTime(){ return requestTime; }
+    public boolean isPending()           { return pending; }
+    public void setPending(boolean p)    { this.pending = p; }
+}
